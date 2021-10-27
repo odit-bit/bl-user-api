d7b628ce464523694db6e29e2e6f9704715185f6
Gema Adhi <geadwi@gmail.com>
2021-10-27 20:32:10 +0700

    change config port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const process = require("process");
 
 const app = express();
-const port = process.env.NODE_PORT;
+const port = process.env.PORT;
 
 //middleware
 app.use(express.json({ limit: "10mb" }));
